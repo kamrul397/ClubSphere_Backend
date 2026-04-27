53a6d038d074d2df0ff6f27dd13dca91448ec0e0
kamrul397 <kamrulislam25262800@gmail.com>
2026-04-28 00:32:49 +0600

    admin routes added

--- index.js
+++ index.js
@@ -5,10 +5,33 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 3000;
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./clubspere_admin_sdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyFBToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).send({ message: "Authorization token is required" });
+  }
+  try {
+    const idToken = token.split(" ")[1]; // Extract the token part after "Bearer"
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    // console.log("decoded token", decodedToken);
+
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "Invalid or expired token" });
+  }
+};
 
 const uri = `mongodb+srv://${
   process.env.DB_USER
@@ -29,9 +52,96 @@ async function run() {
 
     const db = client.db("clubSpere_Database");
     const clubsCollection = db.collection("clubs");
+    const usersCollection = db.collection("users");
+    const clubManagersCollection = db.collection("clubManagers");
+
+    // users api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const email = user.email;
+      const existingUser = await usersCollection.findOne({ email });
+      if (existingUser) {
+        return res.status(400).send({ message: "User already exists" });
+      }
+
+      user.createdAt = new Date();
+      user.role = "member"; // Set default role to "user"
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.patch("/users/:id", async (req, res) => {
+      const { id } = req.params;
+      const { role } = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: role,
+        },
+      };
+      const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    app.get("/users/:email/role", async (req, res) => {
+      const email = req.params.email;
+      const { query } = email;
+      const user = await usersCollection.findOne(query);
+      if (!user) {
+        return res.status(404).send({ message: "User not found" });
+      }
+      res.send({ role: user.role || "member" }); // Default to "member" if role is not set
+    });
+    // club managers api
+
+    app.post("/club-managers", async (req, res) => {
+      const manager = req.body;
+      manager.status = "pending"; // Set default status to "pending"
+      manager.createdAt = new Date();
+      const result = await clubManagersCollection.insertOne(manager);
+      res.send(result);
+    });
+
+    app.get("/club-managers", async (req, res) => {
+      const query = {};
+      if (req.query.status) {
+        query.status = req.query.status;
+      }
+      const result = await clubManagersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.patch("/club-managers/:id", verifyFBToken, async (req, res) => {
+      const status = req.body.status;
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await clubManagersCollection.updateOne(query, updateDoc);
+      if (status === "approved") {
+        const email = req.body.email;
+        const userQuery = { email: email };
+        const userUpdateDoc = {
+          $set: {
+            role: "clubManager",
+          },
+        };
+        await usersCollection.updateOne(userQuery, userUpdateDoc);
+      }
+      res.send(result);
+    });
 
     // clubs api
-    app.get("/clubs", async (req, res) => {
+    app.get("/clubs", verifyFBToken, async (req, res) => {
       const { email } = req.query; // This gets "kamrulislam25262800@gmail.com" from the URL
       let query = {};
 
@@ -53,6 +163,8 @@ async function run() {
       const result = await clubsCollection.insertOne(newClub);
       res.send(result);
     });
+
+    // Delete a club by ID
     app.delete("/clubs/:id", async (req, res) => {
       try {
         const { id } = req.params;
